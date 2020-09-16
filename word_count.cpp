cdfb8b2cac249dc66dc3d91b92f3343e7fea77a2
Shrey Midha <midhashrey@gmail.com>
2020-09-16 19:32:19 +0530

    Ambuj's Essay

---
+++ word_count.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    int i,count=0;
+    string str;
+    cout<<"Enter string:";
+    getline(cin,str);
+    for (i = 0;str[i]!='\0'; i++)
+    {
+        if (str[i]==' ' | str[i]==',' | str[i]=='.' && str[i+1]!='\0')
+        {
+            count++;
+        }
+    }
+    cout<<count+1;    
+    return 0;
+}
